aef8e8daf5f56830f25f9f3da827639ef5359d0c
chaeha <chaeha293@naver.com>
2021-05-20 13:45:27 +0900

    sync

---
+++ src/components/common/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="header">
+    <router-link to="/">
+      <img src="@/assets/logo.png" class="log" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.logo {
+  width: 130px;
+}
+.header {
+  text-align: center;
+  box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.3);
+}
+</style>
