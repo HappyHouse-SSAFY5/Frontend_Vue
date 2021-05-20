97ae1f1b7fc04f6561eee248cbe09a9f74a878bd
LEEYS96 <dldptmf0602@naver.com>
2021-05-20 13:19:14 +0900

    add: vuetify and qna

--- src/components/qna/QnaSearchBar.vue
+++ src/components/qna/QnaSearchBar.vue
@@ -2,7 +2,7 @@
   <b-row class="mt-4 mb-4">
     <b-col class="sm-3" align="left">
       <b-form-input
-        v-model="qnaTitle"
+        v-model="title"
         placeholder="검색어를 입력하세요."
         @keypress.enter="sendKeyword"
       ></b-form-input>
@@ -27,10 +27,12 @@ export default {
   methods: {
     ...mapActions(['getQnaList']),
     sendKeyword() {
+      // 다시 하기
       // this.$emit('send-keyword', this.dongCode);
-      if (this.title) this.getQnaList(this.title);
-      // this.$store.dispatch('getAptList', this.dongCode);
-      this.title = '';
+      
+      // if (this.title) this.getQnaList(this.title);
+      // // this.$store.dispatch('getAptList', this.dongCode);
+      // this.title = '';
     },
     moveForm() {
       this.$router.push('/qna/regist');
