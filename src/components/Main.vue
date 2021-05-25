e485fdb59071998d392f10507ec01a2a028effe3
LEEYS96 <dldptmf0602@naver.com>
2021-05-25 09:19:04 +0900

    WIP: main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="container">
+  <v-container>
+    <v-row class="apt-search">
+      <v-col cols="6" >
+        <div class="border-box">
+          <v-col></v-col>
+          <v-col></v-col>
+          <v-col></v-col>
+          <v-col></v-col>
+          <v-col></v-col>
+          <v-col style="text-align: right; color: white;" class="roboto-bold">Apartment Search</v-col>
+          <v-col style="background-color:white; text-align: left;" class="roboto-bold">서울특별시의 아파트 매매 정보 검색</v-col>
+          <v-col style="background-color:white;"></v-col>
+          <v-col style="background-color:white;"></v-col>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row class="notice">
+      <v-col cols="12">
+        <div class="border-box"></div>
+      </v-col>
+    </v-row>
+    <v-row class="qna">
+      <v-col cols="6">
+        <div class="border-box"></div>
+      </v-col>
+      <v-col cols="6">
+        <div class="border-box"></div>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- <div class="container">
     <div class="item1">최고의 지역을 추천해드립니다!!!!</div>
     <div class="item2">공지사항</div>
     <div class="item3">아파트 매매 정보</div>
     <div class="item4">QNA</div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -14,7 +44,27 @@ export default {
 </script>
 
 <style scoped>
-.container {
+.border-box{
+  border: 8px solid white;
+  border-bottom: 32px solid white;
+  height: 300px;
+}
+.roboto-bold{
+  font-family: 'Roboto';
+  font-weight: 800;
+  font-size: 18px;
+  color: black;
+}
+.apt-search{
+  background-color: #1A237E;
+}
+.notice{
+  background-color: #0D47A1;
+}
+.qna{
+  background-color: #01579B;
+}
+/* .container {
   display: grid;
   margin-top: 3%;
   grid-gap: 10px;
@@ -46,5 +96,5 @@ export default {
 }
 .item4 {
   grid-area: item4;
-}
+} */
 </style>
