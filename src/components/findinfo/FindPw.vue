921f4fe7b002915f8474ceca7055a8b1d779eacc
chaeha <chaeha293@naver.com>
2021-05-27 03:35:32 +0900

    done: Findpw, login, signup

--- src/components/findinfo/FindPw.vue
+++ src/components/findinfo/FindPw.vue
@@ -1,42 +1,54 @@
 <template>
-  <v-app>
-    <div class="findinfo container-fluid">
-      <div class="row">
-        <div class="card col-md-6">
-          <div class="form-group" align="left">
-            <h2 align="center">FIND PASSWORD</h2>
-            <v-text-field v-model="id" label="ID" :disabled="disabled == 1" required></v-text-field>
-            <v-text-field
-              v-model="name"
-              label="Name"
-              :disabled="disabled == 1"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="email"
-              label="E-mail"
-              :disabled="disabled == 1"
-              required
-            ></v-text-field>
-              <button class="col-sm-3" id="btn" align="center" @click="validate">
-                FIND
-              </button>
-          </div>
-        </div>
-        <div class="card answer col-md-6">
-          <div class="pass" v-if="password != '' && password != null">
-            <h3>{{ name }}님의 비밀번호는 {{ password }}입니다.</h3>
-            <br />
-            <button class="col-sm-3"><router-link to="/mypage">비밀번호 변경</router-link></button>
-            <button class="col-sm-2"><router-link to="/login">로그인</router-link></button>
-          </div>
-          <div class="pass" v-else-if="ok && (password === '' || password == null)">
-            <h3>{{ name }}님의 회원 정보가 없습니다.</h3>
-            <br />
-            <button class="col-sm-3"><router-link to="/signup">회원가입</router-link></button>
-          </div>
-        </div>
-      </div>
+  <v-app style="background-color: #1A237E; margin-top: 8px;">
+    <div>
+      <v-form>
+        <v-row>
+          <v-col cols="6">
+            <v-col></v-col> <v-col></v-col> <v-col></v-col>
+            <div class="findpw" align="center">
+              <h2 align="center">FIND PASSWORD</h2>
+              <v-text-field v-model="id" :disabled="disabled == 1" required>
+                <template v-slot:label>
+                  <label style="color: white; margin-left: 3px;">ID</label>
+                </template></v-text-field
+              >
+              <v-text-field v-model="name" :disabled="disabled == 1" required>
+                <template v-slot:label>
+                  <label style="color: white; margin-left: 3px;">Name</label>
+                </template></v-text-field
+              >
+              <v-text-field v-model="email" :disabled="disabled == 1" required>
+                <template v-slot:label>
+                  <label style="color: white; margin-left: 3px;">Email</label>
+                </template></v-text-field
+              >
+              <div class="btnFind">
+                <v-col></v-col>
+                <v-btn class="col-sm-3" id="btn" style="background-color:#1A237E; color:white;" align="center" @click="validate">
+                  FIND
+                </v-btn>
+                <v-col></v-col>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="6">
+            <v-col></v-col><v-col></v-col><v-col></v-col><v-col></v-col><v-col></v-col
+            ><v-col></v-col><v-col></v-col><v-col></v-col>
+            <div class="answer" v-if="ok != null">
+              <div class="pass" v-if="password != '' && password != null">
+                <h3>{{ name }}님의 비밀번호는 {{ password }}입니다.</h3>
+                <br />
+                <v-btn class="col-sm-2" @click="moveLogin">Login</v-btn>
+              </div>
+              <div class="pass" v-else-if="ok && (password === '' || password == null)">
+                <h3>{{ name }}님의 회원 정보가 없습니다.</h3>
+                <br />
+                <v-btn class="col-sm-3" @click="moveSignUp">Sign Up</v-btn>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-form>
     </div>
   </v-app>
 </template>
@@ -75,29 +87,37 @@ export default {
         if (this.password == null || this.password == '') {
           this.ok = true;
         } else {
+          this.ok = false;
           this.disabled = 1;
         }
       });
+    },
+    moveLogin() {
+      this.$router.push('/login');
+    },
+    moveSignUp() {
+      this.$router.push('/signup');
     },
   },
 };
 </script>
 
 <style scoped>
-.findinfo {
-  margin-top: 100px;
-
-}
-.card {
-  border: white;
+.findpw {
+  border: solid 8px white;
+  color: white;
+  margin-left: 10px;
 }
 .answer {
-  margin-top: 50px;
+  color: white;
 }
 #btn {
   border: solid 1px #01579b;
 }
 .button {
   margin: 0 auto;
+}
+.btnFind {
+  background-color: white;
 }
 </style>
